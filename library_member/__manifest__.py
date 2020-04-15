b40cedf62f51dd4f64ed6de6337e4e82281f11b5
2232408653 <2232408653@qq.com>
2020-04-15 11:56:06 +0800

    alanhou book 章节4

---
+++ library_member/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name':'Library Members',
+    'description':'Manage people who will be able to borrow books',
+    'author':'ds',
+    'depends':['library_app','mail'],
+    'data':[
+        'views/book_view.xml',
+        'security/library_security.xml',
+        'security/ir.model.access.csv',
+        'views/member_view.xml',
+        'views/library_menu.xml',
+        'views/book_list_template.xml',
+    ],
+    'application':False,
+}
